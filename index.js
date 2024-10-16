d312de1f98f789e8278055d12631b08c69046e17
Aaron Joshua Emmanuel <ajemmanuel45@outlook.com>
2024-10-16 22:12:22 +0530

    Project Complete

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const e = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -19,10 +20,32 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  const date = req.params.date;
+  let dataObject;
+
+  // Cheaking if a date is given or not
+  if (!date) {
+    dataObject = new Date();
+  } else {
+    // Checking if it is UNIX/String date
+    if (!isNaN(date)) {
+      dataObject = new Date(parseInt(date));  
+    } else {
+      dataObject = new Date(date);  
+    }
+  }
+
+  if (dataObject.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: dataObject.getTime(),
+    utc: dataObject.toUTCString()
+  });
 });
+  
 
 
 
